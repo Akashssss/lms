36a0230714cd2ba3d34b0c6378ac7c5804910c63
Akashssss <akashsingh43457@gmail.com>
2024-04-30 15:44:06 +0530

    update

---
+++ app/(dashboard)/_components/Logo.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export const Logo = () => {
+    return (
+        <div style={{ fontFamily: "Arial, sans-serif" }}>
+            <h1 style={{ fontWeight: "bold", fontSize: "24px", margin: "0" }}>LMS</h1>
+        </div>
+    )
+}
